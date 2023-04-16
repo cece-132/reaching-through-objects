1f44bb5d712566778c04fc4ab4c5225b8dfc3978
Candace Eckels <candace.eckels@gmail.com>
2023-04-15 20:56:13 -0600

    aprtment can list romms alphabetically

--- exercise_1/spec/apartment_spec.rb
+++ exercise_1/spec/apartment_spec.rb
@@ -1,4 +1,5 @@
 require './exercise_1/lib/apartment'
+require './exercise_1/lib/room'
 
 RSpec.describe Apartment do
   
@@ -15,6 +16,40 @@ RSpec.describe Apartment do
       apartment = Apartment.new      
       apartment.rent
       expect(apartment.rented).to be_truthy
+    end
+  end
+
+  describe '#add_room(room_object)' do
+    it 'can add a maximum for 4 rooms to an apartment' do
+      apartment = Apartment.new
+      bathroom = Room.new("bathroom")
+      apartment.add_room(bathroom)
+      apartment.add_room(Room.new("laundry"))
+      apartment.add_room(Room.new("kitchen"))
+      apartment.add_room(Room.new("bedroom"))
+
+      expect(apartment.rooms).to be_a Array
+      expect(apartment.rooms.count).to eq 4
+    end
+  end
+
+  describe '#list_rooms_by_name_alphabetically' do
+    it 'list rooms in alpha order asc' do
+      apartment = Apartment.new
+      bathroom = Room.new("bathroom")
+      laundry = Room.new("laundry")
+      kitchen = Room.new("kitchen")
+      bedroom = Room.new("bedroom")
+
+      apartment.add_room(bathroom)
+      apartment.add_room(laundry)
+      apartment.add_room(kitchen)
+      apartment.add_room(bedroom)
+
+      expected = [bathroom, bedroom, kitchen, laundry]
+
+      expect(apartment.list_rooms_by_name_alphabetically).to be_a Array
+      expect(apartment.list_rooms_by_name_alphabetically).to eq(expected)
     end
   end
 end
